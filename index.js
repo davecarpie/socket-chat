b260a5bac7cc289f8878c0f20b470b97cad7fc50
Dave Carpenter <davecar@microsoft.com>
2018-05-13 21:29:16 -0700

    nicks sorta work

--- index.js
+++ index.js
@@ -6,14 +6,36 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
+var nicks = {}
+
+let getNick = function(id) {
+    if (nicks.id) {
+        return nicks.id
+    } else {
+        return (id.substring(id.length - 5));
+    }
+}
+
 io.on('connection', function(socket) {
     console.log('a user connected');
     socket.broadcast.emit('user connected')
 
     socket.on('chat message', function(msg) {
         console.log(msg);
-        socket.broadcast.emit('chat message', msg)
+        var nick = getNick(socket.id)
+        var data = { "sender" : nick, "msg" : msg}
+        socket.broadcast.emit('chat message', data)
     })
+
+    
+
+    socket.on('nick', function(data) {
+        console.log("new nick" + data)
+        let previousNick = getNick(socket.id)
+        nicks[socket.id] = data
+        var msg = { "sender" : null, "msg" : previousNick + " changed their name to " + data }
+        io.emit('chat message', msg)
+    });
 
     socket.on('disconnect', function() {
         console.log('user disconnected');
