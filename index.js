7589a842c4ba118f26656d0e12abe5d4624ef437
Dave Carpenter <davecar@microsoft.com>
2018-05-14 21:17:38 -0700

    typing indicator works locally

--- index.js
+++ index.js
@@ -6,31 +6,45 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
-var nicks = {}
+var users = {}
 
 io.on('connection', function(socket) {
 
     socket.on('joined', (name) =>
     {
-        nicks[socket.id] = name
+        users[socket.id] = {}
+        users[socket.id].nick = name
         socket.broadcast.emit('user connected', name)    
     })
 
     socket.on('msg', function(msg) {
-        var nick = nicks[socket.id]
+        var nick = users[socket.id].nick
         var data = { "sender" : nick, "text" : msg}
         socket.broadcast.emit('msg', data)
     })
 
     socket.on('nick', function(name) {
-        let previousNick = nicks[socket.id]
-        nicks[socket.id] = name
+        let previousNick = users[socket.id].nick
+        users[socket.id].nick = name
         var msg = { "sender" : null, "text" : previousNick + " changed their name to " + name }
         socket.broadcast.emit('msg', msg)
     });
 
     socket.on('disconnect', function() {
         console.log('user disconnected');
+    })
+
+    socket.on('typing', function(isTyping) {
+        users[socket.id].typing = isTyping
+        typingUsers = []
+        for (userId in users) {
+            if (users.hasOwnProperty[userId]) {
+                if (users.userId.typing) {
+                    typingUsers += [users.userId.nick]
+                }
+            }
+        }
+        io.emit('typists', typingUsers)
     })
 })
 
